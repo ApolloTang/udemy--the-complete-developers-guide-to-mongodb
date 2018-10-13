58ee7abae815ad89469282a28113015d590d7602
Apollo Tang <tang.apollo@gmail.com>
2018-10-13 06:44:36 -0400

    use async await

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -4,6 +4,7 @@ const User = require('../src/user');
 console.log('... In reading_test.js')
 
 describe('Reading users out of the database', ()=>{
+
   let joe;
 
   beforeEach( done => {
@@ -13,21 +14,23 @@ describe('Reading users out of the database', ()=>{
 
   it('Finds all users with a name of Joe', done => {
 
-    User.find({ name: 'Joe'})
-      .then(
-        user => {
-          console.log('xxxx user:', user);
-          console.log('xxxx joe:', joe);
+    User.find({ name: 'Joe'}).then( user => {
 
-          // assert( user[0]._id === joe._id ) // <-- This will fail !!!
+      // assert( user[0]._id === joe._id ) //@ <-- This will fail !!!
 
-          //@ _id must parse to string
-          assert( user[0]._id.toString() === joe._id.toString() );
-          done();
-        }
-      )
+      //@ To be able to compare with '===', _id must be parsed to string
+      assert( user[0]._id.toString() === joe._id.toString() );
+      done();
+    });
 
   })
 
+  it('Find a user with a particular id', async (/* done */) => { //@ <-- don't pass in 'done' if use async
+
+    const user = await User.findOne( {_id: joe._id} );
+    assert( user._id.toString() === joe._id.toString() );
+    // done() //@ <----------- don't use done because we are using async await
+
+  });
 
 })
